ab7300da66c6d02d9cfa49b6d64c735e98760790
Naman Singhal <91019897+Naman4925@users.noreply.github.com>
2021-12-02 20:33:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -81,5 +81,5 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('.guess').value = '';
 
   document.querySelector('body').style.backgroundColor = 'rgb(22, 219, 245)';
-  d.querySelector('.number').style.width = '15rem';
+  do.querySelector('.number').style.width = '15rem';
 });
